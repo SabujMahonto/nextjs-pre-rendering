70ff3ccd4c85d77174bd50aa253d5dfe5324f7a6
sabujmahonto <sabujmahonto@gmail.com>
2023-04-26 11:32:58 +0600

    server side rendering SSR getSErverSideProps useing done

---
+++ pages/framework/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Framework({frameworks}) {
+    
+  return (
+    <div>
+        <h2>All Framework</h2>
+        {
+        frameworks.map(framework =>(
+            
+            <div key={framework.id}>
+            <h3>{framework.id}-{framework.title},-{framework.core}</h3>
+            </div>
+        ))
+
+        }
+    </div>
+  )
+}
+
+export async function getServerSideProps(){
+    const res = await fetch(" http://localhost:4000/frameworks");
+    const data = await res.json();
+
+    return{
+        props:{
+            frameworks : data,
+        }
+    }
+}
